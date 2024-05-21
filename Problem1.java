2afa7d8b8b3f8b63eba8dd40e1cf282af881cb83
pujakumari <puja.kumari@sjsu.edu>
2024-05-20 18:44:23 -0700

    Completed Competitive-Coding-1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,39 @@
+//Problem Name: Interview Problem: Find Missing Number in a sorted array
 
+public class Problem1 {
+
+    // TC - O(log n)
+    // SC - O(1)
+
+    public static int findMissingNo(int[] nums) {
+
+        if(nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int low =0;
+        int high = nums.length-1;
+
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+
+            if(nums[mid] != mid+1) {
+                if(mid == low || nums[mid-1] == mid)
+                    return mid+1;
+                else
+                    high = mid-1;
+            }
+
+            else {
+                low = mid+1;
+            }
+        }
+
+        return low+1;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {1, 2, 3, 4, 5, 6, 7};
+        System.out.println("Missing No = " + findMissingNo(nums));
+    }
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,132 @@
+//Problem Name: Interview Problem: Design Min Heap
+//TC - getMin() - O(1), insert() - O(log n), remove() - O(log n)
+public class Problem2 {
 
+    int[] minHeap;
+    int size;
+    int maxSize;
+
+    public Problem2(int maxSize) {
+        size = -1;
+        this.maxSize = maxSize;
+        this.minHeap = new int[maxSize + 1];
+    }
+
+    //function to get the parent for this pos(child)
+    public int getParent(int pos) {
+
+        return (pos-1)/2;
+    }
+
+    //function to left get child for this pos(parent)
+    public int getLeftChild(int pos) {
+
+        return 2*pos + 1;
+    }
+
+    //function to left get child for this pos(parent)
+    public int getRightChild(int pos) {
+
+        return 2*pos + 2;
+    }
+
+    //function to check if the node is a leaf node
+    public boolean isLeafNode(int pos) {
+        return pos >= size / 2;
+    }
+
+    //function to swap elements
+    public void swap(int pos1, int pos2) {
+
+        int temp = minHeap[pos1];
+        minHeap[pos1] = minHeap[pos2];
+        minHeap[pos2] = temp;
+    }
+
+    //heapify function to maintain the heap property if an element is deleted
+    public void heapify(int pos) {
+        if(!isLeafNode(pos)) {
+            //Check if this pos is greater than its left or right child, swap this position with min(leftChild, rightChild)
+            int leftPos = getLeftChild(pos);
+            int rightPos = getRightChild(pos);
+            if(minHeap[pos] > minHeap[leftPos] || minHeap[pos] > minHeap[rightPos]) {
+                int swapPos = (minHeap[leftPos] <= minHeap[rightPos])? leftPos : rightPos;
+                swap(pos, swapPos);
+            }
+        }
+    }
+
+    //function to insert an element into the heap
+    public void insert(int item) {
+        if(size >= maxSize)
+            return;
+
+        minHeap[++size] = item;
+
+        //if item value is less than its parent, then swap the parent with item and check other elements upwards
+        int current = size;
+        while(item < minHeap[getParent(current)]) {
+            swap(current, getParent(current));
+            current = getParent(current);
+        }
+    }
+
+    //function to get min element
+    public int getMin() {
+        if(size == 0)
+            return Integer.MIN_VALUE;
+        return minHeap[0];
+    }
+
+    //function to delete element from minHeap and return the min element
+    public int remove() {
+        int popped = minHeap[0];
+        minHeap[0] = minHeap[size--];
+        heapify(0);
+        return popped;
+    }
+
+    public void print()
+    {
+        for (int i = 0; i < size / 2; i++) {
+
+            System.out.print(
+                    " PARENT : " + minHeap[i]
+                            + " LEFT CHILD : " + minHeap[2 * i + 1]
+                            + " RIGHT CHILD :" + minHeap[2 * i + 2]);
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Problem2 minHeap = new Problem2(10);
+
+        minHeap.insert(5);
+        minHeap.insert(3);
+        minHeap.insert(17);
+        minHeap.insert(10);
+        minHeap.insert(84);
+        minHeap.insert(19);
+        minHeap.insert(6);
+        minHeap.insert(22);
+        minHeap.insert(9);
+
+        minHeap.print();
+
+        //Get Min element
+        System.out.println("Min element =" + minHeap.getMin());
+
+        //Remove 3
+        System.out.println("Removed element =" + minHeap.remove());
+
+        //Get Min element
+        System.out.println("Min element =" + minHeap.getMin());
+
+        //Insert 1
+        minHeap.insert(1);
+
+        //Get Min element
+        System.out.println("Min element =" + minHeap.getMin());
+    }
+}
